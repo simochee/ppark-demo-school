f40e118ddf7d0a479056b4eccffc36e74c1480fa
Ryoya Tamura <simochee@gmail.com>
2017-07-06 12:44:25 +0900

    ルーター書こうとした

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,51 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+/**
+ * ホームページ
+ */
+router.get('/', (req, res, next) => {
   res.render('client/index');
 });
+
+/**
+ * サインイン
+ */
+router.get('/signin', (req, res, next) => {
+  res.render('clinet/signin');
+});
+
+/**
+ * サインイン処理
+ */
+router.post('/signin', (req, res, next) => {
+  // some processes
+});
+
+/**
+ * サインアップ
+ */
+router.get('/signup', (req, res, next) => {
+  res.render('client/signup');
+});
+
+/**
+ * サインアップ処理
+ */
+router.post('/signup', (req, res, next) => {
+  // some processes
+});
+
+/**
+ * サインイン認証
+ */
+router.use((req, res, next) => {
+  next();
+});
+
+/**
+ * 入庫先
+ */
+
 
 module.exports = router;
